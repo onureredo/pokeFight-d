9f28e122dfe8861cf194587f1a95059f364ad224
Onur Eredo <onureredo@yahoo.com>
2023-02-02 12:34:02 +0100

    render pokemon details

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -8,14 +8,22 @@ function Pokemon({ pokemonId }) {
         axios
             .get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
             .then(response => {
-                console.log(response.data);
-
+                // console.log(response.data);
+                setPokemon(response.data);
             });
     }, [pokemonId]);
 
-
     return (
-        <div></div>
+        <div className='pokemon-card'>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} />
+            <p>HP: {pokemon.stats[0].base_stat}</p>
+            <p>Attack: {pokemon.stats[1].base_stat}</p>
+            <p>Defense: {pokemon.stats[2].base_stat}</p>
+            <p>Speed: {pokemon.stats[5].base_stat}</p>
+            <p>XP: {pokemon.base_experience}</p>
+
+        </div>
     )
 }
 
