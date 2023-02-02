acde3a6da0cb9cc75cea41b6287f70181885a53d
Onur Eredo <onureredo@yahoo.com>
2023-02-02 12:55:16 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Pokemon from "./components/Pokemon";
+import VS from "./assets/images/vs.png"
 
 function App() {
   return (
     <div className="App">
+      {/* <Pokemon pokemonId={25} /> */}
       <Pokemon pokemonId={Math.floor(Math.random() * 1000)} />
-
+      <img style={{ height: "200px" }} src={VS} alt="vs" />
+      <Pokemon pokemonId={Math.floor(Math.random() * 1000)} />
     </div>
   );
 }
