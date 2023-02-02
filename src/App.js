254f786bed9dd86a10271c57c8e0d38d9fdeab6f
Onur Eredo <onureredo@yahoo.com>
2023-02-02 12:27:17 +0100

    axios fetch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Pokemon from "./components/Pokemon";
 function App() {
   return (
     <div className="App">
+      <Pokemon pokemonId={1} />
 
     </div>
   );
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,7 +1,17 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 
-function Pokemon() {
+function Pokemon({ pokemonId }) {
+    const [pokemon, setPokemon] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+            .then(response => {
+                console.log(response.data);
+
+            })
+    })
 
 
     return (
